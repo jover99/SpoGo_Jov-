51a7742e7c7d155b9a3f8c13aa9fb8363ec11769
Richard Jove <jover@bc.edu>
2019-12-01 10:05:27 -0500

    Initial Commit

---
+++ SpoGo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SpoGo
+//
+//  Created by Richard Jove on 12/1/19.
+//  Copyright © 2019 Richard Jove. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
